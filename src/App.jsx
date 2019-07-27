4cb386e0da6ad72eb543e0729081b74ac8ae9364
openopen <openopen114@gmail.com>
2019-07-27 11:10:49 +0800

    Grid demo2

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React, { Fragment } from "react";
-import GridDemo1 from "./Grid/demo1/demo1";
+// import GridDemo1 from "./Grid/demo1/demo1";
+import GridDemo2 from "./Grid/demo2/demo2";
 
 import "./App.css";
 
 function App() {
   return (
     <Fragment>
-      <GridDemo1 />
+      {/* <GridDemo1 /> */}
+      <GridDemo2 />
     </Fragment>
   );
 }
---
+++ src/Grid/demo2/demo2.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "./demo2.css";
+
+export default class GridDemo2 extends Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="AA">AA 1/2</div>
+        <div className="BB">BB 2/3</div>
+        <div className="CC">CC 3/4</div>
+        <div className="DD">DD 4/5</div>
+      </div>
+    );
+  }
+}
