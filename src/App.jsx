df962d71d9a3e2815b0f9f1579e62e605e5d34c7
openopen <openopen114@gmail.com>
2019-07-27 10:06:06 +0800

    Grid Demo1

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React, { Fragment } from "react";
+import GridDemo1 from "./Grid/demo1/demo1";
+
+import "./App.css";
+
+function App() {
+  return (
+    <Fragment>
+      <GridDemo1 />
+    </Fragment>
+  );
+}
+
+export default App;
---
+++ src/Grid/demo1/demo1.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "./demo1.css";
+
+export default class GridDemo1 extends Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="AA">AA</div>
+        <div className="BB">BB</div>
+        <div className="CC">CC</div>
+        <div className="DD">DD</div>
+      </div>
+    );
+  }
+}
